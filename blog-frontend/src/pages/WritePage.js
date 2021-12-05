96d090b079c18bbc5bddd8642dca76fa16d52bca
ijaesin <ijaesinii@gmail.com>
2021-12-06 00:11:27 +0900

    Implement of writing function at the front end

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Responsive from '../components/common/Responsive';
+import WriteActionButtonContainer from '../containers/write/WriteActionButtonContainer';
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
 
 function WritePage() {
-  return <div>Write Page</div>;
+  return (
+    <Responsive>
+      <EditorContainer />
+      <TagBoxContainer />
+      <WriteActionButtonContainer />
+    </Responsive>
+  );
 }
 
 export default WritePage;
